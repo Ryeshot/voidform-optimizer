62b70edd30379563a3ae52bfdaeeafd99a004b8c
Ryeshot <31656086+Ryeshot@users.noreply.github.com>
2020-05-28 00:28:59 -0400

    Beginning to bind shift

--- src/components/panels/AbilityKeybindsPanel.js
+++ src/components/panels/AbilityKeybindsPanel.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect, useReducer, useRef} from 'react';
 import Panel from "./Panel"
+import {keybinds} from "../../utils/constants"
 
 import "./Panel.css"
 import "./Tooltip.css"
@@ -11,16 +12,19 @@ const AbilityKeybindsPanel = (props) => {
     const panel = "abilitykeybinds"
     const header = "Ability and Keybinds"
     const panelClass = "bottom-panel"
+    const modifiers = ["Shift", "Alt", "Control"]
 
     const defaultState = {
         tooltip: "",
         keybindText: "",
-        currentAbility: ""
+        currentAbility: "",
+        modifier: ""
     }
 
     const [tooltip, setTooltip] = useState(defaultState.tooltip)
     const [keybindText, setKeybindText] = useState(defaultState.keybindText)
     const [currentAbility, setCurrentAbility] = useState(defaultState.currentAbility)
+    const [modifier, setModifier] = useState(defaultState.modifier)
 
     const handleClose = () => {
         setTooltip(defaultState.tooltip)
@@ -48,17 +52,47 @@ const AbilityKeybindsPanel = (props) => {
 
         setKeybindText(`Press any key to bind to ${abilities[ability].displayName}`)
 
-        document.addEventListener("keyup", bindAbility, {once: true})
+        document.addEventListener("keypress", bindAbility, {once: true})
+        document.addEventListener("keydown", bindModifier, {once: true})
+        document.addEventListener("keyup", unbindModifier, {once: true})
+    }
+
+    const bindModifier = (event) => {
+        //console.log(event.key)
+        if(modifiers.includes(event.key)) {
+            setModifier(m => event.key)
+            document.addEventListener("keydown", bindAbilityWithModifier)
+        }
+    }
+
+    const unbindModifier = () => {
+        if(modifier) setModifier(m => defaultState.modifier)
+    }
+
+    const bindAbilityWithModifier = (event) => {
+        if(!modifiers.includes(event.key)) {
+            document.removeEventListener("keyup", bindAbility)
+            document.removeEventListener("keydown", bindAbilityWithModifier)
+
+            let key = modifier[0] + "-" + (event.key.match(/[a-zA-Z]/) ? event.key.toUpperCase() : event.key)
+            let ability = currentAbilityRef.current 
+    
+            onKeybind(event.key, ability)
+            setKeybindText(`${abilities[ability].displayName} successfuly bound to ${key}`)
+        }
     }
 
     const bindAbility = (event) => {
-        document.removeEventListener("keyup", bindAbility)
+        document.removeEventListener("keypress", bindAbility)
         onPause(false)
+        let eventKey = event.key
 
-        let key = event.key.match(/[a-zA-Z]/) ? event.key.toUpperCase() : event.key
+        if(keybinds[event.key]) eventKey = keybinds[event.key]
+
+        let key = eventKey.match(/[a-zA-Z]/) ? eventKey.toUpperCase() : eventKey
         let ability = currentAbilityRef.current 
 
-        onKeybind(event.key, ability)
+        onKeybind(eventKey, ability)
         setKeybindText(`${abilities[ability].displayName} successfuly bound to ${key}`)
     }
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,5 @@
 export const interval = 20
 export const lag = 50
+export const keybinds = {
+    "!": "S-1"
+}
