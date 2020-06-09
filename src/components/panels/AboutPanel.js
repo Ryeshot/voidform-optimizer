b7b927ead3238deb47247fd52e4198d13ad3477d
Ryeshot <31656086+Ryeshot@users.noreply.github.com>
2020-06-09 17:01:21 -0400

    Add about panel content

--- src/components/panels/AboutPanel.js
+++ src/components/panels/AboutPanel.js
@@ -14,7 +14,24 @@ const AboutPanel = (props) => {
         <Panel panel={panel} onClick={onClick} handleClose={closePanel} header={header} panelClass={panelClass}
             style={{transform: `translateY(${currentPanel === panel ? "0px": "-685px"}`}}
         >
-            <div className="panel-content-header">About</div>
+            <div className="middle-panel-content">
+                <div className="panel-content-header">About</div>
+                <div className="medium-text left-text panel-text-content">
+                    <p>
+                    Voidform Optimizer is a thought experiment I had.
+                    </p>
+                    <p>
+                    I am convinced that many, although not all, players' grievances with Voidform are caused by the
+                    implementations of Voidform they have experienced, and not by the design of Voidform itself. Therefore I have made it my mission to allow others
+                    to experience Voidform in a myriad of ways, in hope that they will find what they most enjoy about it, share it with others, and maybe as a
+                    community come to an agreement on the best ways to make Voidform be the best it can be.
+                    </p>
+                    <p>
+                    If you have any questions, feedback, or bugs to report, please feel free to DM me on Discord at Shot#1711.
+                    </p>
+
+                </div>
+            </div>
         </Panel>      
     )
 }
