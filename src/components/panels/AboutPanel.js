96972b48d9b9e347fced16781d7e7a7d00e9ef30
Ryeshot <31656086+Ryeshot@users.noreply.github.com>
2020-06-24 19:27:29 -0400

    Update about panel content

--- src/components/panels/AboutPanel.js
+++ src/components/panels/AboutPanel.js
@@ -23,8 +23,8 @@ const AboutPanel = (props) => {
                     <p>
                     I am convinced that many, although not all, players' grievances with Voidform are caused by the
                     implementations of Voidform they have experienced, and not by the design of Voidform itself. Therefore I have made it my mission to allow others
-                    to experience Voidform in a myriad of ways, in hope that they will find what they most enjoy about it, share it with others, and maybe as a
-                    community come to an agreement on the best ways to make Voidform be the best it can be. <b>#savevoidform</b>
+                    to experience Voidform in a myriad of ways; in hope that they realize it isn't inherently flawed, find what they most enjoy about it, and 
+                    share those findings with others. Then, just maybe, as a community we can come to an agreement on how to <b>#savevoidform</b>.
                     </p>
                     <p>
                     If you have any questions, feedback, or bugs to report, please feel free to DM me on Discord at Shot#1711.
