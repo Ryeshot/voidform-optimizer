d6c2af197f51de9cc331ea8bd47dcff62555a830
Ryeshot <31656086+Ryeshot@users.noreply.github.com>
2020-06-25 02:12:13 -0400

    Add what's new dialogue

---
+++ src/lib/whatsNew.js
@@ -0,0 +1,10 @@
+export default [
+    {
+        header: "Devouring Plague",
+        description: "The recent Shadowlands Devouring Plague addition is now available to try out!"
+    },
+    {
+        header: "What's New?",
+        description: "Added a what's new dialogue to inform about recent changes to Voidform Optimizer."
+    }
+]
