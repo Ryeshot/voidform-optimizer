495137956d5aeb4da833fc9b72d6581c2ad7f848
Ryeshot <31656086+Ryeshot@users.noreply.github.com>
2020-07-15 12:56:39 -0400

    Devouring plague now fires cooldown events

--- src/lib/abilityEvents.js
+++ src/lib/abilityEvents.js
@@ -69,6 +69,8 @@ const abilityEvents = {
     },
     "devouring-plague": {
         onEvent: {
+            "COOLDOWN_START": ["ABILITY_COOLDOWN_START"],
+            "COOLDOWN_END": ["ABILITY_COOLDOWN_END"],
             "CAST_START": ["ABILITY_CAST_START"],
             "CAST_SUCCESS": ["ABILITY_CAST_SUCCESS", "AURA_START", "RESOURCE_UPDATE"]
         }
