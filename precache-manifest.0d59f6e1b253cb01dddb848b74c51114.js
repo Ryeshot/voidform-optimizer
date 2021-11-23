802e4753dd99fc3e4b2e4dd47f0ed8fa7c962371
Ryeshot <Ryeshot@users.noreply.github.com>
2021-11-23 20:53:18 +0000

    deploy: 7bf02bf884b59bc5169a7b80932b1e9dd25035ad

---
+++ precache-manifest.0d59f6e1b253cb01dddb848b74c51114.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7149640addd79c0ad3d83ff504e6af78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e609ad85280936c42807",
+    "url": "/static/css/main.3efb12d3.chunk.css"
+  },
+  {
+    "revision": "7c9ed415a134dd8e1dae",
+    "url": "/static/js/2.d1ed30a1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d1ed30a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e609ad85280936c42807",
+    "url": "/static/js/main.aa79461a.chunk.js"
+  },
+  {
+    "revision": "08ee3beca3524d7cc602",
+    "url": "/static/js/runtime-main.830bf87c.js"
+  },
+  {
+    "revision": "f383588ed2bde8ac191535823f55a240",
+    "url": "/static/media/tentacle-bottom.f383588e.png"
+  },
+  {
+    "revision": "c0309d4fb0b75f7d52c0228f9828cdc0",
+    "url": "/static/media/tentacle-heading.c0309d4f.png"
+  },
+  {
+    "revision": "0dedc097f308e779b4c0db316c02d39f",
+    "url": "/static/media/tentacle-left.0dedc097.png"
+  },
+  {
+    "revision": "d7d9eaa54445f01e637c12597260407b",
+    "url": "/static/media/tentacle-right.d7d9eaa5.png"
+  },
+  {
+    "revision": "676087a248360009d8ffa74672fcfba8",
+    "url": "/static/media/tentacle-small.676087a2.png"
+  },
+  {
+    "revision": "f30c7ee4124ed42726efcdd8b60c2021",
+    "url": "/static/media/theme.f30c7ee4.png"
+  }
+]);
